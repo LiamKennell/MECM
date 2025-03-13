f48eacfbf6628b6179982e2ad7134e47f904d3a2
Liam <liam.kennell@proton.me>
2025-03-13 10:17:51 +0000

    VsCode recommendations

--- GetFailedDistributions.ps1
+++ GetFailedDistributions.ps1
@@ -1,8 +1,8 @@
 $SiteCode = "PR0"
-$failures = Get-WmiObject -Namespace root\sms\site_$SiteCode -Query "SELECT * FROM SMS_PackageStatusDistPointsSummarizer WHERE State <> 0" |
-    Select ServerNALPath,LastCopied,PackageType,State,PackageID,SummaryDate |
+#$failures = Get-WmiObject -Namespace root\sms\site_$SiteCode -Query "SELECT * FROM SMS_PackageStatusDistPointsSummarizer WHERE State <> 0" |
+    Select-Object ServerNALPath,LastCopied,PackageType,State,PackageID,SummaryDate |
         ForEach-Object {
-            $PKG = Get-WmiObject -NameSpace root\sms\site_$SiteCode -Class SMS_Packagebaseclass -Filter "PackageID = '$($_.PackageID)'" | Select Name,PackageSize
+            $PKG = Get-WmiObject -NameSpace root\sms\site_$SiteCode -Class SMS_Packagebaseclass -Filter "PackageID = '$($_.PackageID)'" | Select-Object Name,PackageSize
             $server = $_.ServerNALPath.Split("\\")[2]
             $size = $PKG.PackageSize / 1KB
             $State =  switch ($_.State)
@@ -44,7 +44,7 @@ $failures = Get-WmiObject -Namespace root\sms\site_$SiteCode -Query "SELECT * FR
                 SummaryDate = $SummaryDate
                 }
         } |
-            Select Name,'PackageSize (MB)',PackageType,PackageID,State,StateCode,DistributionPoint,LastCopied,SummaryDate | Sort LastCopied -Descending |
+            Select-Object Name,'PackageSize (MB)',PackageType,PackageID,State,StateCode,DistributionPoint,LastCopied,SummaryDate | Sort-Object LastCopied -Descending |
                 Out-GridView -Title "Select package/s to redistribute" -OutputMode Multiple |
                  ForEach-Object {
                     Get-WmiObject -Namespace root\sms\site_$SiteCode -Query "SELECT * FROM SMS_DistributionPoint WHERE PackageID='$($_.PackageID)' and ServerNALPath like '%$($_.DistributionPoint)%'" |
